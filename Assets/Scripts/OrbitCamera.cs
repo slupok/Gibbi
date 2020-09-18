e63cd620e05a3456a62a46f9d5aa84754170996b
Slupoke <slupoke@yandex.ru>
2020-09-18 20:38:15 +0300

    1.изменил управление и вид. 2.добавил ассет текстур

---
+++ Assets/Scripts/OrbitCamera.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitCamera : MonoBehaviour
+{
+    [SerializeField] private Transform _player; 
+    private Transform _target; 
+    public float RotSpeed = 1.5f;
+    private float _rotX;
+    private float _rotY;
+    
+    public float MinimumY = -40F;
+    public float MaximumY = 30F;
+    
+    private Vector3 _offset;
+    void Start() {
+        if (_player == null)
+        {
+            _target.position = new Vector3(0,0,0);
+            return;
+        }
+        SetTarget(_player);
+        _rotY = transform.eulerAngles.y;
+        _rotX = transform.eulerAngles.x;
+    
+        _offset = _target.position - transform.position; 
+    }
+    void LateUpdate() 
+    {
+        _rotY += Input.GetAxis("Mouse Y") * RotSpeed * 3;
+        _rotX += Input.GetAxis("Mouse X") * RotSpeed * 3;
+        
+        _rotY = ClampAngle(_rotY, MinimumY, MaximumY);
+        
+        Quaternion rotation = Quaternion.Euler(-_rotY, _rotX, 0);
+        transform.position = _target.position - (rotation * _offset);
+        transform.LookAt(_target); 
+    }
+
+    public void SetTarget(Transform target)
+    {
+        _target = target;
+    }
+    public static float ClampAngle (float angle, float min, float max)
+    {
+        angle = angle % 360;
+        if ((angle >= -360F) && (angle <= 360F)) {
+            if (angle < -360F) {
+                angle += 360F;
+            }
+            if (angle > 360F) {
+                angle -= 360F;
+            }			
+        }
+        return Mathf.Clamp (angle, min, max);
+    }
+}
---
+++ Assets/Scripts/RelativeMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))] 
+public class RelativeMovement : MonoBehaviour
+{
+    [SerializeField] private Transform _targetCamera; 
+    public Animator Animator;
+    public float RotSpeed = 15.0f;
+    public float moveSpeed = 6.0f;
+    private CharacterController _characterController;
+    private bool _runFlag;
+
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+    void Update() {
+        Vector3 movement = Vector3.zero;
+        float horInput = Input.GetAxis("Horizontal") ;
+        float vertInput = Input.GetAxis("Vertical");
+        if (horInput != 0 || vertInput != 0) 
+        {
+            Animator.SetBool("IsRun",true);
+            _runFlag = true;
+            
+            movement.x = horInput * moveSpeed;
+            movement.z = vertInput * moveSpeed;
+            movement = Vector3.ClampMagnitude(movement, moveSpeed);
+            Quaternion tmp = _targetCamera.rotation; 
+            _targetCamera.eulerAngles = new Vector3(0, _targetCamera.eulerAngles.y, 0);
+            movement = _targetCamera.TransformDirection(movement); 
+            _targetCamera.rotation = tmp;
+            Quaternion direction = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Lerp(transform.rotation,direction, RotSpeed * Time.deltaTime);
+        }
+        else
+        {
+            if (_runFlag)
+            {
+                Animator.SetBool("IsRun",false);
+                _runFlag = false;
+            }
+        }
+        movement *= Time.deltaTime; 
+        _characterController.Move(movement);
+    }
+}
